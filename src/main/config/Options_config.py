a3e1853ae2476d34cf7eb0160bb2915e312282f3
Trần Thị Phương Anh <phuonganh.tran@vmg.vn>
2025-04-25 17:02:27 +0700

    Update safari options

--- src/main/config/Options_config.py
+++ src/main/config/Options_config.py
@@ -9,34 +9,40 @@ class OptionsConfiguration:
         # Thiết lập Chrome options
         options = ChromeOptions()
         options.add_argument('--no-sandbox')
-        options.add_argument('--headless')
+        # options.add_argument('--headless')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--window-size=1920,1080')
+
         return options
     
     def firefox_options(self):
         # Thiết lập Firefox options
         options = FirefoxOptions()
         options.add_argument('--no-sandbox')
-        options.add_argument('--headless')
+        # options.add_argument('--headless')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--window-size=1920,1080')
+
         return options
     
     def edge_options(self):
         # Thiết lập Edge options
         options = EdgeOptions()
         options.add_argument('--no-sandbox')
-        options.add_argument('--headless')
+        # options.add_argument('--headless')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--window-size=1920,1080')
+
         return options
     
     def safari_options(self):
         # Thiết lập Safari options
         options = SafariOptions()
-        options.add_argument('--no-sandbox')
-        options.add_argument('--headless')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--window-size=1920,1080')
+        options.set_window_rect(0, 0, 1920, 1080)
+        options.browser_version('latest')
+        options.use_technology_preview(True)
+        options.automatic_inspection(False)
+        options.automatic_profiling(False)
+        options.enable_downloads(False)
+
         return options
